26bb835ecc3f51964f046b1d2f77ad0ce9fd57e9
Gildas Montcho <gmontcho@gmail.com>
2024-05-31 22:39:40 +0200

    feat(main.py): create a main.py function and test a TextNode

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+
+def main():
+    print("running main.py")
+    text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(text_node)
+
+
+main()
