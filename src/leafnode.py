e13e91ab4ccfc858915b85e0180c104d1358c78f
Gildas Montcho <gmontcho@gmail.com>
2024-06-03 21:11:33 +0200

    feat(leafnode.py): another way of instanciating LeafNodes
    a small modification of the call of the parent (HTMNNode) constructor

--- src/leafnode.py
+++ src/leafnode.py
@@ -3,7 +3,7 @@ from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        super().__init__(tag, value, props=props)
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
         if self.value is None:
