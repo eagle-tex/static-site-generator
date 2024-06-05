409f606e8987b1271a5f00f0edfee0297bfde17c
Gildas Montcho <gmontcho@gmail.com>
2024-06-06 01:43:50 +0200

    feat(main.py): create ParentNode instances and print their to_html() method

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from leafnode import LeafNode
+from parentnode import ParentNode
 from textnode import TextNode
 from htmlnode import HTMLNode
 
@@ -54,6 +55,72 @@ def main():
     print(leafnode2)
     print(leafnode1.to_html())
     print(leafnode2.to_html())
+    print()
+
+    parent_node1 = ParentNode(
+        "p",
+        [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ],
+    )
+    print(parent_node1.to_html())
+
+    parent_node2 = ParentNode(
+        "div",
+        [
+            ParentNode(
+                "div",
+                [
+                    ParentNode(
+                        "ul",
+                        [
+                            LeafNode("li", "Hyper Focus"),
+                            LeafNode("li", "Deep Work"),
+                            LeafNode("li", "Ultra Learning"),
+                        ],
+                    )
+                ],
+            ),
+            LeafNode("span", "Colored text"),
+            LeafNode(None, "Ordinary text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ],
+    )
+    print(parent_node2.to_html())
+    print()
+
+    nested_node = ParentNode(
+        "div",
+        [
+            ParentNode(
+                "p",
+                [
+                    LeafNode("b", "Bold inside inner paragraph"),
+                    LeafNode(None, "Normal inside inner paragraph"),
+                ],
+            ),
+            LeafNode(None, "Normal text inside outer div"),
+        ],
+    )
+    print(nested_node.to_html())
+    print()
+
+    one_child_node = ParentNode(
+        "ul",
+        [
+            ParentNode(
+                "li",
+                [
+                    LeafNode(None, "Single list item"),
+                ],
+            ),
+        ],
+    )
+    print(one_child_node.to_html())
 
 
 main()
