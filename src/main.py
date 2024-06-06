51c7aeb99e483da056f89af4c5a89801fb4ada28
Gildas Montcho <gmontcho@gmail.com>
2024-06-06 23:58:37 +0200

    feat(main.py): create TextNode instances and test the text_node_to_html method

--- src/main.py
+++ src/main.py
@@ -6,9 +6,28 @@ from htmlnode import HTMLNode
 
 def main():
     print("running main.py")
-    text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(text_node)
+    print("**************** TEXT NODES *******************")
+    text_node = TextNode("This is a TEXT text node", "text")
+    print(text_node.text_node_to_html_node().to_html())
+    bold_text_node = TextNode("This is a BOLD text node", "bold")
+    print(bold_text_node.text_node_to_html_node().to_html())
+    italic_text_node = TextNode("This is a ITALIC text node", "italic")
+    print(italic_text_node.text_node_to_html_node().to_html())
+    code_text_node = TextNode("This is a CODE text node", "code")
+    print(code_text_node.text_node_to_html_node().to_html())
+    link_text_node = TextNode(
+        "This is a LINK text node", "link", "https://www.boot.dev"
+    )
+    print(link_text_node.text_node_to_html_node().to_html())
+    img_text_node = TextNode(
+        "This is a IMAGE text node", "image", "/assets/background1.jpg"
+    )
+    print(img_text_node.text_node_to_html_node().to_html())
+    # pdf_text_node = TextNode("This is a PDF text node", "pdf")
+    # print(pdf_text_node.text_node_to_html_node())
+    print()
 
+    print("**************** HTML NODES *******************")
     html_node1 = HTMLNode(
         "div",
         "My link",
@@ -34,7 +53,6 @@ def main():
         },
     )
     print(html_node1)
-
     print()
 
     html_node2 = HTMLNode(
@@ -48,6 +66,9 @@ def main():
         },
     )
     print(html_node2)
+    print()
+
+    print("**************** LEAF NODES *******************")
 
     leafnode1 = LeafNode("p", "This is a paragraph of text.")
     leafnode2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
@@ -56,6 +77,8 @@ def main():
     print(leafnode1.to_html())
     print(leafnode2.to_html())
     print()
+
+    print("**************** PARENT NODES *******************")
 
     parent_node1 = ParentNode(
         "p",
