e62378cba4a2c99177c0eaba8ec82fef468fca78
Gildas Montcho <gmontcho@gmail.com>
2024-06-13 11:43:40 +0200

    feat(main.py): play with the `split_nodes_delimiter` function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 from leafnode import LeafNode
 from parentnode import ParentNode
-from textnode import TextNode
+from textnode import TextNode, split_nodes_delimiter
 from htmlnode import HTMLNode
 
 
-def main():
-    print("running main.py")
+def play_with_text_nodes():
     print("**************** TEXT NODES *******************")
     text_node = TextNode("This is a TEXT text node", "text")
     print(text_node.text_node_to_html_node().to_html())
@@ -27,6 +26,8 @@ def main():
     # print(pdf_text_node.text_node_to_html_node())
     print()
 
+
+def play_with_html_nodes():
     print("**************** HTML NODES *******************")
     html_node1 = HTMLNode(
         "div",
@@ -68,6 +69,8 @@ def main():
     print(html_node2)
     print()
 
+
+def play_with_leaf_nodes():
     print("**************** LEAF NODES *******************")
 
     leafnode1 = LeafNode("p", "This is a paragraph of text.")
@@ -78,6 +81,8 @@ def main():
     print(leafnode2.to_html())
     print()
 
+
+def play_with_parent_nodes():
     print("**************** PARENT NODES *******************")
 
     parent_node1 = ParentNode(
@@ -144,6 +149,27 @@ def main():
         ],
     )
     print(one_child_node.to_html())
+
+
+def play_with_split_delimiter():
+    text_type_text = "text"
+    text_type_bold = "bold"
+    text_type_code = "code"
+    text_type_italic = "italic"
+    node1 = TextNode("This is text with a `code block` word", text_type_text)
+    new_nodes1 = split_nodes_delimiter([node1], "`", text_type_code)
+    for node in new_nodes1:
+        print(f'Content: "{node.text}", Type: {node.text_type}')
+    # print(new_nodes1)
+
+
+def main():
+    print("running main.py")
+    # play_with_text_nodes()
+    # play_with_html_nodes()
+    # play_with_leaf_nodes()
+    # play_with_parent_nodes()
+    play_with_split_delimiter()
 
 
 main()
