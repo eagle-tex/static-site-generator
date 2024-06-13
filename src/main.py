8ac3a1c69cd8b11d0084b46cc10e65ec1fb6efbb
Gildas Montcho <gmontcho@gmail.com>
2024-06-13 15:18:53 +0200

    feat(main.py): play with the extract_markdown_{images,links} functions

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from leafnode import LeafNode
 from parentnode import ParentNode
 from textnode import TextNode
-from inline import split_nodes_delimiter
+from inline import (
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_delimiter,
+)
 from htmlnode import HTMLNode
 
 
@@ -154,14 +158,24 @@ def play_with_parent_nodes():
 
 def play_with_split_delimiter():
     text_type_text = "text"
-    text_type_bold = "bold"
+    # text_type_bold = "bold"
     text_type_code = "code"
-    text_type_italic = "italic"
+    # text_type_italic = "italic"
     node1 = TextNode("This is text with a `code block` word", text_type_text)
     new_nodes1 = split_nodes_delimiter([node1], "`", text_type_code)
     for node in new_nodes1:
         print(f'Content: "{node.text}", Type: {node.text_type}')
     # print(new_nodes1)
+
+
+def play_with_extract_md_img():
+    text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
+    print(extract_markdown_images(text))
+
+
+def play_with_extract_md_links():
+    text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+    print(extract_markdown_links(text))
 
 
 def main():
@@ -170,7 +184,10 @@ def main():
     # play_with_html_nodes()
     # play_with_leaf_nodes()
     # play_with_parent_nodes()
-    play_with_split_delimiter()
+    # play_with_split_delimiter()
+    play_with_extract_md_img()
+    print()
+    play_with_extract_md_links()
 
 
 main()
