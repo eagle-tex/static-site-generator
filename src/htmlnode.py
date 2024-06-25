b28cab5558b214145c6f0a77ba0fafa6cde5bc9e
Gildas Montcho <gmontcho@gmail.com>
2024-06-25 22:49:51 +0200

    feat(htmlnode.py): update the __repr__ method
    displays nested HTML elements
    <div>
      <h3>Heading level 3</h3> if total length <= 80
    </div>
    otherwise
    <div>
      <h3>
        The length of this heading is superior to the maximum value set of 80
      </h3>
    </div>

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,7 +17,19 @@ class HTMLNode:
         return res
 
     def __repr__(self):
-        SELF_CLOSING_TAGS = ["img", "input"]
+        SELF_CLOSING_TAGS = [
+            "area",
+            "base",
+            "br",
+            "col",
+            "embed",
+            "hr",
+            "img",
+            "input",
+            "keygen",
+            "link",
+        ]
+        MAX_LENGTH = 80  # maximum length of an element if displayed on one line
 
         def disp_node(node, level=1):
             lines = []
@@ -29,27 +41,60 @@ class HTMLNode:
                 tag = root_node_info[0]
                 if tag in SELF_CLOSING_TAGS:
                     opening_tag = pad_str(f"<{tag}{props}/>", level - 1)
-                else:
+                elif tag:  # tag is not empty nor None
                     opening_tag = pad_str(f"<{root_node_info[0]}{props}>", level - 1)
-                lines.append(opening_tag)
+                else:  # tag is empty or None
+                    opening_tag = ""
 
                 value = root_node_info[1]
-                if value:
-                    value = pad_str(value, level)
-                    lines.append(value)
+                if value:  # value != ''
+                    if tag:  # tag != ''
+                        value = pad_str(value, level)
+                    else:  # tag is falsy ('' or None. '' in this case, since tag is a string)
+                        value = pad_str(value, level - 1)
 
-                if isinstance(node.children, list):
+                if isinstance(node.children, list) and node.children:
+                    if tag in SELF_CLOSING_TAGS:
+                        raise Exception(f"<{tag}> tag cannot have children")
+                    if opening_tag:  # opening_tag != ''
+                        closing_tag = pad_str(f"</{root_node_info[0]}>", level - 1)
+                        if len(opening_tag) + len(value.strip()) <= MAX_LENGTH - 7:
+                            lines.append(f"{opening_tag}{value.strip()}")
+                        else:
+                            lines.append(opening_tag)
+                            lines.append(value)
+                    else:  # opening_tag == ''
+                        lines.append(value)
+
                     for node_child_el in node.children:
+                        # recursively extend the lines array with each child nested inside (hence: level+1)
                         lines.extend(disp_node(node_child_el, level + 1))
-                elif node.children is None:
-                    pass
 
-                if tag not in SELF_CLOSING_TAGS:
                     closing_tag = pad_str(f"</{root_node_info[0]}>", level - 1)
                     lines.append(closing_tag)
+                else:  # node does not have any children (node.children == [] or None)
+                    if opening_tag:  # opening_tag != ''
+                        if tag not in SELF_CLOSING_TAGS:
+                            closing_tag = pad_str(f"</{root_node_info[0]}>", level - 1)
+                            if (
+                                len(opening_tag)
+                                + len(value.strip())
+                                + len(closing_tag.strip())
+                                <= MAX_LENGTH
+                            ):
+                                lines.append(
+                                    f"{opening_tag}{value.strip()}{closing_tag.strip()}"
+                                )  # all on one line
+                            else:
+                                # put opening_tag, value and closing_tag each on its line
+                                lines.append(opening_tag)
+                                lines.append(value)
+                                lines.append(closing_tag)
+                    else:  # opening_tag == ''
+                        # there are no opening nor closing tags. just add a line with value.
+                        lines.append(value)
 
-            # if the node is not an HTMLNode
-            else:
+            else:  # if the node is not an HTMLNode
                 raise Exception("The argument of disp_node MUST be of type 'HTMLNode'")
 
             return lines
@@ -59,7 +104,7 @@ class HTMLNode:
 
 def get_node_info(html_node):
     if isinstance(html_node, HTMLNode):
-        tag = f"{html_node.tag}"
+        tag = f"{html_node.tag}" if html_node.tag else ""
         value = f"{html_node.value}" if html_node.value else ""
         props = f"{html_node.props}" if html_node.props else ""
 
