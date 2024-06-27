f53c0b3b26fdf36ae3310c11d0e4f645462e2f6c
Gildas Montcho <gmontcho@gmail.com>
2024-06-27 10:02:00 +0200

    fix(test_parentnode.py): fix broken tests after update of __repr__ method

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -1,4 +1,5 @@
 import unittest
+from htmlnode import flatten_html_element
 from parentnode import ParentNode
 from leafnode import LeafNode
 
@@ -14,8 +15,10 @@ class Test_ParentNode(unittest.TestCase):
                 LeafNode(None, "Normal text"),
             ],
         )
+        result1 = parent_node1.to_html()
+        flat_result1 = flatten_html_element(result1)
         expected1 = "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>"
-        self.assertEqual(parent_node1.to_html(), expected1)
+        self.assertEqual(flat_result1, expected1)
 
     def test_nested_node1_to_html(self):
         nested_node1 = ParentNode(
@@ -40,8 +43,10 @@ class Test_ParentNode(unittest.TestCase):
                 LeafNode(None, "Normal text"),
             ],
         )
+        result = nested_node1.to_html()
+        flat_result = flatten_html_element(result)
         expected = "<div><div><ul><li>Hyper Focus</li><li>Deep Work</li><li>Ultra Learning</li></ul></div><span>Colored text</span>Ordinary text<i>italic text</i>Normal text</div>"
-        self.assertEqual(nested_node1.to_html(), expected)
+        self.assertEqual(flat_result, expected)
 
     def test_nested_node2_to_html(self):
         nested_node2 = ParentNode(
@@ -57,12 +62,17 @@ class Test_ParentNode(unittest.TestCase):
                 LeafNode(None, "Normal text inside outer div"),
             ],
         )
+        result = nested_node2.to_html()
+        flat_result = flatten_html_element(result)
         expected = "<div><p><b>Bold inside inner paragraph</b>Normal inside inner paragraph</p>Normal text inside outer div</div>"
-        self.assertEqual(nested_node2.to_html(), expected)
+        self.assertEqual(flat_result, expected)
 
     def test_edge_case(self):
         one_child_node = ParentNode(
             "ul", [ParentNode("li", [LeafNode(None, "Single list item")])]
         )
+        result = one_child_node.to_html()
+        flat_result = flatten_html_element(result)
         expected = "<ul><li>Single list item</li></ul>"
-        self.assertEqual(one_child_node.to_html(), expected)
+
+        self.assertEqual(flat_result, expected)
